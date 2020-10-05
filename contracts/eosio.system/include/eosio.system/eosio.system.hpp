4368d058cc387f20c2917b25e2e6c392272cc0fa
smartbitcoin <smartbitcoin@gmail.com>
2020-10-05 17:45:09 -0400

    blocktime to 1 sec, min activated_stake as 10% of axis 24m.

--- contracts/eosio.system/include/eosio.system/eosio.system.hpp
+++ contracts/eosio.system/include/eosio.system/eosio.system.hpp
@@ -59,20 +59,36 @@ namespace eosiosystem {
          return ( flags & ~static_cast<F>(field) );
    }
 
-   static constexpr uint32_t seconds_per_year      = 52 * 7 * 24 * 3600;
-   static constexpr uint32_t seconds_per_day       = 24 * 3600;
    static constexpr uint32_t seconds_per_hour      = 3600;
+   static constexpr uint32_t seconds_per_day       = 24 * seconds_per_hour;
+   static constexpr uint32_t seconds_per_year      = 52 * 7 * seconds_per_day;
+
    static constexpr int64_t  useconds_per_year     = int64_t(seconds_per_year) * 1000'000ll;
    static constexpr int64_t  useconds_per_day      = int64_t(seconds_per_day) * 1000'000ll;
    static constexpr int64_t  useconds_per_hour     = int64_t(seconds_per_hour) * 1000'000ll;
-   static constexpr uint32_t blocks_per_day        = 2 * seconds_per_day; // half seconds per day
 
-   static constexpr int64_t  min_activated_stake   = 150'000'000'0000;
-   static constexpr int64_t  ram_gift_bytes        = 1400;
+   // must be consitens with os/libraries/chain/include/eosio/chain/config.hpp
+   // line 38:
+   // const static int      block_interval_ms = 1000;
+   //static constexpr uint32_t blocks_per_day        = 2 * seconds_per_day; // half seconds per day
+   static constexpr uint32_t blocks_per_day        = seconds_per_day; // half seconds per day
+
+   // @TODO:
+   // need change according to axis's total supply. which is 24M.
+   // 1 billion eos at ICO,  4 decimal place -> 150'000'000 / 1B -> 15%
+   // Axis as major supply is reserved,  the threshhold defined as 10%
+   static constexpr int64_t  min_activated_stake   = 2'400'000;
+   //static constexpr int64_t  min_activated_stake   = 150'000'000'0000;
+
    static constexpr int64_t  min_pervote_daily_pay = 100'0000;
+
+
+   static constexpr int64_t  ram_gift_bytes        = 1400;
    static constexpr uint32_t refund_delay_sec      = 3 * seconds_per_day;
 
    static constexpr int64_t  inflation_precision           = 100;     // 2 decimals
+
+
    static constexpr int64_t  default_annual_rate           = 500;     // 5% annual rate
    static constexpr int64_t  pay_factor_precision          = 10000;
    static constexpr int64_t  default_inflation_pay_factor  = 50000;   // producers pay share = 10000 / 50000 = 20% of the inflation
